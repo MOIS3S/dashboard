c6d0f6ec226584c74727099e3dce126a7a499cba
Moises Castillo <moisescastillo20@gmail.com>
2023-05-22 09:13:30 +0200

    feat: add page User to root path

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Users from './pages/Users';
 
 const Wrapper = styled.div`
 
@@ -12,20 +12,7 @@ export function App() {
       <Routes>
         <Route
           path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
+          element={<Users/>}
         />
       </Routes>
       {/* END: routes */}
