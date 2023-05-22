feaa8d1b01fc1b23805bfd748e19099bbeb88971
Moises Castillo <moisescastillo20@gmail.com>
2023-05-22 09:04:58 +0200

    feat: adding new service to get list of users

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ListAllEntities } from './dto/ListAllEntities ';
+const data = require('../../../../DB.json');
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to backend!' };
+  findAll(page: string): ListAllEntities {
+    const parsedPage = parseInt(page);
+    const PAGE_SIZE = 10;
+    const lastIndex = parsedPage * PAGE_SIZE;
+    const firstIndex = lastIndex - PAGE_SIZE;
+    const currentData = data.slice(firstIndex, lastIndex);
+
+    return {
+      totalItems: data.length,
+      currentPage: parsedPage,
+      totalPages: Math.ceil(data.length / PAGE_SIZE),
+      data: currentData,
+    };
   }
 }
